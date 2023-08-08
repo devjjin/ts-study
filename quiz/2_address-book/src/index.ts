ccba8dcc6021177c673054a8ab45b7386fe7f329
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-08 16:38:21 +0900

    feat : type modules

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,21 +1,4 @@
-// 어떤 키값이 오든 내부 num의 속성은 number오도록
-interface PhoneNumberDictionary {
-  [phone: string]: {
-    num: number;
-  };
-}
-
-interface Contact {
-  name: string;
-  address: string;
-  phones: PhoneNumberDictionary;
-}
-
-enum PhoneType {
-  Home = 'home',
-  Office = 'office',
-  Studio = 'studio',
-}
+import { Contact, PhoneType } from './types';
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
---
+++ quiz/2_address-book/src/types.ts
@@ -0,0 +1,20 @@
+// 어떤 키값이 오든 내부 num의 속성은 number오도록
+interface PhoneNumberDictionary {
+  [phone: string]: {
+    num: number;
+  };
+}
+
+interface Contact {
+  name: string;
+  address: string;
+  phones: PhoneNumberDictionary;
+}
+
+enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
+export { Contact, PhoneType };
