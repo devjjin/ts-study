0de3789a0271d61c802d5976911ed7b21d500ca9
devjjin <38846447+devjjin@users.noreply.github.com>
2023-08-09 10:29:10 +0900

    feat : add exmaple of utility type with partial

--- class-note/ts-modules/13_utility-type.ts
+++ class-note/ts-modules/13_utility-type.ts
@@ -7,7 +7,7 @@ interface Product {
     stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
+// 1. 상품 목록을 받아오기 위한 API 함수
 function fetchProducts(): Promise<Product[]> {
     // ..
 }
@@ -19,7 +19,8 @@ function fetchProducts(): Promise<Product[]> {
 //     price: number;    
 // }
 
-// 상품 상세 API 함수 (Product 인터페이스의 일부만 필요함)
+// 2. 특정 상품 상세 정보를 나타내기 위한 API 함수 
+// Product 인터페이스의 일부만 필요함
 // function displayProductDetail(shoppingItem : { id: number; name: string; price: number}) {
 // function displayProductDetail(shoppingItem: ProductDetail) {
 
@@ -27,4 +28,19 @@ function fetchProducts(): Promise<Product[]> {
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>;
 function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
 
+}
+
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+// Partial : Product 인터페이스의 모든 속성을 optinal로 처리해 선택적으로 사용 가능
+type UpdateProduct = Partial<Product>;
+// 3. 특정 상품 정보를 업데이트(갱신)하는 API 함수
+function updateProductItem(productItem: Partial<Product>) {
+    
 }
