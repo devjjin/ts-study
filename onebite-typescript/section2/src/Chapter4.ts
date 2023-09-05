7033403627bd4cb5c0e4bbdc159455a089c47aa7
devjjin <38846447+devjjin@users.noreply.github.com>
2023-09-05 18:29:29 +0900

    docs : object와 객체리터럴 타입

--- onebite-typescript/section2/src/Chapter4.ts
+++ onebite-typescript/section2/src/Chapter4.ts
@@ -1,33 +1,48 @@
-// object 타입
-let user: object = {
-  id: 1,
-  name: "김수진",
-}
-
-// 프로퍼티에 접근 시 오류!
-user.id; // ❌ object형식에 id 속성이 없습니다.?
-// 왜 나는 정의했는데, 이 값이 객체다라는 정보 외에는 아무런 정보가 없는 타입이므로,
-// 프로퍼티, 메서드에 무엇이 있는지 알 수 없으므로
-
-// 그렇다면? 객체리터럴 타입을 활용해라.
-
-// 객체리터럴 타입
-// {}를 사용해 프로퍼티의 타입을 정의해서 아래 쓰이는 것을 리터럴하는것 과 같음
-let user1: {
+// 1. 타입별칭(Type Alias)
+// type 타입이름 = 타입 형태로 타입 정의
+type User = {
   id: number;
   name: string;
-} = {
-  id: 1,
-  name: "김수진"
-}
-user1.id;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
 
-let dog: {
-  name: string;
-  color: string;
-} = {
-  name: "뚱뚱이",
-  color: "white",
+// type User = {}; // ❌ 동일한 스코프에 동일한 이름의 type 별칭 선언은 불가능함
+
+function test() {
+  type User = string; // 함수 내부에서는 string 타입, 외부에서는 객체타입
 }
 
-dog.color;
+// 타입 관련 문법은 컴파일과 함께 모두 사라짐 (타입 별칭 포함)
+
+// 2. 인덱스 시그니처(Index Signature)
+// 객체 타입을 유연하게 정의할 수 있도록 돕는 특수한 문법
+type CountryCodes = {
+	Korea: string;
+	UnitedState: string;
+	UnitedKingdom: string;
+  // (... 100개 국가)
+};
+
+let countryCodes: CountryCodes = {
+	Korea: "ko",
+	UnitedState: "us",
+	UnitedKingdom: "uk",
+  // (... 100개 국가)
+};
+
+// 만약 countryCodes에 100개의 프로퍼티(국가코드가) 추가되어야한다면?
+// 프로퍼티를 모두 정의해줘야하므로 번거로움!!!
+// 이럴때 인덱스시그니처 사용하면 간편함!
+
+// [key : string] : string    시그니처 문법 사용
+// : 객체 타입에는 key가 stinrg타입, value가 string 타입인 모든 프로퍼티들이 포함된다라는 의미
+
+type CountryNumberCodes = {
+  [key: string] : number;
+  Korea: number; // 이때 반드시 포함해야 하는 프로퍼티가 있다면 다음과 같이 직접 명시해도 됨
+  // Korea: string; // ❌ 오류! 시그니처의 value타입(number)와 직접 추가한 프로퍼티 value 타입이 호환되거나 일치해야하므로
+
+}
